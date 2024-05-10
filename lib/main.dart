3066481ea91211e5e3dbd5648ab5c642b6b82490
Iammaro <maro.h.mourad@gmail.com>
2024-05-10 14:22:33 +0300

    update text

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
-
-
 import '/packages/packages_import.dart';
 
-void main() => runApp( const MyApp());
+void main() => runApp(const MyApp());
 
-
-
+//hello omar azzam #1 SE
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
       home: Home(),
     );
